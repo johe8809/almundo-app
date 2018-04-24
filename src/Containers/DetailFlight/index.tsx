b767a48c65a02281a90e02409e52cc6a9c4648c8
Yonnathan Henao <yonnathan.henao@almundo.com>
2018-04-24 15:13:04 -0500

    Se realiza componente para ver el detalle de un vuelo

---
+++ src/Containers/DetailFlight/index.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './style';
+
+export default class DetailFlight extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+  render() {
+    const { destination, dateDeparture, hourDeparture, origin, dateArrival, hourArrival } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.flightDatetime}>
+          <Text style={styles.iata}>{destination.iata}</Text>
+          <Text style={styles.datetime}>{`Sale ${dateDeparture}`}</Text>
+          <Text style={styles.datetime}>{hourDeparture}</Text>
+        </View>
+        {/* <View style={styles.arrow}>
+        <IconEndtypo name={'arrow-long-right'} color="rgba(97, 97, 97, 1)" size={24}
+          style={{ textAlign: 'center' }} />
+      </View> */}
+        <View style={styles.flightDatetime}>
+          <Text style={styles.iata}>{origin.iata}</Text>
+          <Text style={styles.datetime}>{`Llega ${dateArrival}`}</Text>
+          <Text style={styles.datetime}>{hourArrival}</Text>
+        </View>
+      </View>
+    );
+  }
+};
